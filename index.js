4167a68dec049d340be77aa1dd4f48b0ef12bc17
Fernando Serpa <serpaef@outlook.com>
2022-01-24 15:37:56 -0300

    impl: GET route "/ping"

--- index.js
+++ index.js
@@ -4,5 +4,8 @@ require('dotenv').config();
 
 const app = express();
 
+// this is almost a hello world to an API coding
+app.get('/ping', (_req, res) => res.status(200).json({message: 'pong!'}));
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
