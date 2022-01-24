92301917b1620446189801e4bf8c92c0b70732f5
Fernando Serpa <serpaef@outlook.com>
2022-01-24 15:47:14 -0300

    impl: route GET "/cep/:cep"

--- index.js
+++ index.js
@@ -4,5 +4,9 @@ require('dotenv').config();
 
 const app = express();
 
+app.get('/cep/:cep', (req, res) => {
+  return res.status(200).json({ message: 'WIP'});
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
