acdcc11e3ec245a7d9511f1e95fa6ae9d07728a9
Fernando Serpa <serpaef@outlook.com>
2022-01-24 21:46:25 -0300

    impl: validation on cep format

--- index.js
+++ index.js
@@ -5,7 +5,12 @@ require('dotenv').config();
 const app = express();
 
 app.get('/cep/:cep', (req, res) => {
-  return res.status(200).json({ message: 'WIP'});
+  const { cep } = req.params;
+  // Is `cep` in a valid format?
+  const cepFormat = /\d{5}-?\d{3}/;
+  if(cepFormat.test(cep)) return res.status(200).json({ message: 'CEP is valid!'})
+  
+  return res.status(400).json({ message: 'try a valid CEP format'});
 });
 
 const PORT = process.env.PORT || 3000;
